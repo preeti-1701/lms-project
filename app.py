30f54c797f1afcc2fdb2212122ea53aff903924e
sush-4321 <153520237+sush-4321@users.noreply.github.com>
2026-04-12 11:27:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print("preeti")
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Dummy users
+users = [
+    {"email": "student@gmail.com", "password": "1234", "role": "student"},
+    {"email": "admin@gmail.com", "password": "admin123", "role": "admin"}
+]
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    for user in users:
+        if user["email"] == email and user["password"] == password:
+            return jsonify({
+                "message": "Login successful",
+                "role": user["role"]
+            }), 200
+
+    return jsonify({"message": "Invalid credentials"}), 401
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
