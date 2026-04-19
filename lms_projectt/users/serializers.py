ce8dca773e9dda81a88c1b8ee3edae9b8a8d35f2
ManoJTN <manojtn8618@gmail.com@gmail.com>
2026-04-19 22:04:22 +0530

    updated LMS backend logic

---
+++ lms_projectt/users/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Course, Assignment
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class AssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
+        fields = '__all__'
