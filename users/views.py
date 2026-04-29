188ae76d33bc413c5d8e51be1a7de1388f51f8b1
Selvkarthik <sselvkarthik@gmail.com>
2026-04-29 22:24:21 +0530

    Added frontend pages and working user registration system

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .models import CustomUser
 
 # Create your views here.
+def login_view(request):
+    return render(request, 'users/login.html')
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        mobile = request.POST.get("mobile")
+        password = request.POST.get("password")
+
+        if CustomUser.objects.filter(username=username).exists():
+            return render(request, "users/register.html", {"error": "Username already exists"})
+
+        user = CustomUser.objects.create_user(
+            username=username,
+            email=email,
+            mobile=mobile,
+            password=password
+        )
+
+        login(request, user)
+        return redirect("home")
+    return render(request, 'users/register.html')
