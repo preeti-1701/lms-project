1a5dd5215fb97539d10e4a366904adcd0c177c75
Selvkarthik <sselvkarthik@gmail.com>
2026-04-30 22:48:14 +0530

    Updated course views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Course
 
 # Create your views here.
+@login_required
+def course_list(request):
+    if request.user.role == 'student':
+        courses = request.user.enrolled_courses.all()
+    else:
+        courses = Course.objects.all()
+    return render(request, 'courses/course_list.html', {'courses': courses})
