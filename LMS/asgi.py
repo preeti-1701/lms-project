814a1127497c5f561b0ad29cd0df7f0db6afc971
rakshitharakshu524-glitch <rakshitharakshu524@gmail.com>
2026-05-01 14:45:08 +0530

    Add files via upload

--- LMS/asgi.py
+++ LMS/asgi.py
@@ -1,4 +1,3 @@
-"""
 ASGI config for LMS project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
--- LMS/urls.py
+++ LMS/urls.py
@@ -1,22 +1,23 @@
-"""
-URL configuration for LMS project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+from courses.models import Course
+from users.models import Student
+
+def home(request):
+    return render(request, 'home.html')
+
+def courses_view(request):
+    courses = Course.objects.all()
+    return render(request, 'courses.html', {'courses': courses})
+
+def users_view(request):
+    users = Student.objects.all()
+    return render(request, 'users.html', {'users': users})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('courses/', courses_view),
+    path('users/', users_view),
 ]
