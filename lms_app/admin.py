1994b85e36a9d5d834dc08ce382f020eba775bc8
Asha <ashagowda2244@gmail.com>
2026-05-01 20:46:12 +0530

    Updated My LMS Project

--- lms_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Course, Video, CourseEnrollment
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Course)
-admin.site.register(Video)
-admin.site.register(CourseEnrollment)
