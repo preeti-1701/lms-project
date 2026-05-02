3a8bf3009a5c785c9fed93703e57d81a184ba1b6
ManoJTN <manojtn8618@gmail.com>
2026-05-02 19:57:13 +0530

    test commit

--- lms_projectt/users/views.py
+++ lms_projectt/users/views.py
@@ -36,7 +36,7 @@ def register(request):
             password=password
         )
 
-        # force normal user
+        # force normal use
         user.is_staff = False
         user.is_superuser = False
         user.save()
