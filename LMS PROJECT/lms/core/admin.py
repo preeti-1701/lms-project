692a952f0ec985c579784823f6b20bdcc9769b59
PAVANKUMARBM <thenameispavan029@gmail.com>
2026-05-07 00:28:46 +0530

    Updated LMS project

--- LMS PROJECT/lms/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Course, Video, Profile, Enrollment
-
-admin.site.register(Course)
-admin.site.register(Video)
-admin.site.register(Profile)
-admin.site.register(Enrollment)
